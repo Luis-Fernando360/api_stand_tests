f10495b0fbc368a284b506962ebdcac1d592bdef
Luis Fernando López G <fer_logu@hotmail.com>
2025-11-10 13:56:09 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,13 @@
+URL_SERVICE = "https://cnt-ab015f86-1c4a-4889-b470-4abdcafd1425.containerhub.tripleten-services.com"
+
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+LOG_TEST_PATH = "/logs/test/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
